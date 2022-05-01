5530dba27b5e18cb13c5bb84ffcfd5de3910d83a
Kelvin Faria <contato.kelvinfaria@gmail.com>
2022-05-01 16:51:12 -0300

    navigation module created

--- buildSrc/src/main/java/CostumerLibs.kt
+++ buildSrc/src/main/java/CostumerLibs.kt
@@ -2,4 +2,6 @@ object CostumerLibs {
 
     const val featureAuthentication = ":costumer:feature:authentication"
     const val featureHome = ":costumer:feature:home"
+
+    const val navigation = ":costumer:navigation"
 }
